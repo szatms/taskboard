5d7f443e1a6ecf3a63d9d80e3951504d84000a94
Szabo Tamas <szabo.tamas.20040922@gmail.com>
2025-11-08 22:04:09 +0100

    Backend mostly done

--- src/main/java/hu/SzaboTamas/taskboard/controller/TaskController.java
+++ src/main/java/hu/SzaboTamas/taskboard/controller/TaskController.java
@@ -1,5 +1,63 @@
 package hu.SzaboTamas.taskboard.controller;
 
-public class TaskController {
+import hu.SzaboTamas.taskboard.model.Task;
+import hu.SzaboTamas.taskboard.service.TaskService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "*")
+public class TaskController {
+    private final TaskService service;
+
+    public TaskController(TaskService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Task> allTasks() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task newTask(@RequestBody Task task) {
+        return service.save(task);
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return service.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found")
+                );
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        Task existing = service.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found")
+                );
+
+        existing.setTitle(updatedTask.getTitle());
+        existing.setDescription(updatedTask.getDescription());
+        existing.setStatus(updatedTask.isStatus());
+
+        return service.save(existing);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTask(@PathVariable Long id) {
+        if (service.findById(id).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found");
+        }
+        service.delete(id);
+    }
 }
